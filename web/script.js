eb5bbf8b7cf73b471e1733ae9f99ebec662f5cd0
bohmid <elbo7ilahi@gmail.com>
2026-03-14 15:06:46 +0100

    [fix] replaced OK btns and replace it by CONFIRM

---
+++ web/script.js
@@ -0,0 +1,4 @@
+//script to check both players entered their names
+const message = document.getElementById("msg");
+message.textContent =
+  "PLEASE ENTER YOUR NAMES THEN CLICK ON THE BUTTON TO CONFIRM";
